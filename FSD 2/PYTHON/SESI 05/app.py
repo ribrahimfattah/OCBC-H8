302bef1166bcd860d17d2cd861e6e1d07cedd001
Rasyid Ibrahim Fattah <r.ibrahimfattah@gmail.com>
2021-11-12 08:40:44 +0700

    sesi 3-7

---
+++ FSD 2/PYTHON/SESI 05/app.py
@@ -0,0 +1,45 @@
+class Dog():    # class name PascalCase
+
+
+
+
+raka = [
+    [0] "Raka Ardhi", 
+    [1] 28, 
+    [2] "CurDev", 
+    [3] 2265
+]
+
+spock = [
+    [0] "Spock", 
+    [1] 35, 
+    [2] "Science Officer", 
+    [3] 2254
+]
+
+mccoy = [
+    [0] "Leonard McCoy", 
+    [1] "Chief Medical Officer", 
+    [2] 2266
+]
+
+class name: Employee
+properties : name, age, position, year_started
+behavior : ()
+
+object : raka, spock, mccoy
+
+n = value
+ClassName(asds, kjl)
+
+def function_name():
+
+class ClassName():
+    ...
+    properties via __init__ method(self, prop1, prop2)
+        .. = prop1
+        .. = prop2
+
+    method definition
+
+ClassName(prop1, prop2)
